3de89c2b8f10dc049bebd03e349dbcaa81a3f089
farhan sharif <farhansharif1410@gmail.com>
2022-10-29 20:24:19 +0530

    post server added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -10,9 +10,37 @@ const Header = () => {
             .then(data => setUser(data))
     }, [])
 
+    const handleClick = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        form.reset();
+        const user = { email, name }
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                const currUser = [...users, data]
+                setUser(currUser)
+            })
+            .catch(error => console.error(error))
+        console.log(user);
+    }
+
     return (
         <div>
-            <InputField />
+            <form onSubmit={handleClick}>
+                <input type="text" name="name" id="" placeholder='name' /> <br />
+                <input type="email" name='email' placeholder='Email' /> <br />
+                <button>Click here</button>
+            </form>
             <h1>User length {users.length}</h1>
             <div>
                 {
--- src/component/InputField/InputField.jsx
+++ src/component/InputField/InputField.jsx
@@ -2,23 +2,11 @@ import React from 'react';
 
 const InputField = () => {
 
-    const handleClick = (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const passeord = form.password.value;
-        form.reset();
-        const info = { email, passeord }
-        console.log(info);
-    }
+
 
     return (
         <div>
-            <form onSubmit={handleClick}>
-                <input type="email" name='email' placeholder='Email' /> <br />
-                <input type="password" name="password" id="" placeholder='passeord' /> <br />
-                <button>Click here</button>
-            </form>
+
         </div>
     );
 };
